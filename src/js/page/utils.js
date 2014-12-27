8cd5a793e2ec81d3717658dcf7a6a2ba3136b6f9
Jake Archibald <jaffathecake@gmail.com>
2014-12-27 13:40:02 +0000

    Moving test SVGs out

--- src/js/page/utils.js
+++ src/js/page/utils.js
@@ -16,6 +16,27 @@
   return r;
 };*/
 
+exports.get = function get(url) {
+  return new Promise(function(resolve, reject) {
+    var req = new XMLHttpRequest();
+    req.open('GET', url);
+
+    req.onload = function() {
+      if (req.status == 200) {
+        resolve(req.response);
+      }
+      else {
+        reject(Error(req.statusText));
+      }
+    };
+    req.onerror = function() {
+      reject(Error("Network Error"));
+    };
+    
+    req.send();
+  });
+}
+
 exports.strToEl = (function () {
   var tmpEl = document.createElement('div');
   return function (str) {
