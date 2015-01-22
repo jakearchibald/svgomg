5c85125ba546e70553ae603688877a4cee93f6b7
Jake Archibald <jaffathecake@gmail.com>
2015-01-22 14:26:09 +0000

    Compressing SVGs

--- src/js/page/ui/download-button.js
+++ src/js/page/ui/download-button.js
@@ -2,12 +2,12 @@ var utils = require('../utils');
 
 class DownloadButton {
   constructor() {
-    this.container = utils.strToEl(`
-      <a href="./" class="floating-action-button">
-        <title>Download output</title>
-        <svg viewBox="0 0 24 24" class="icon"><path d="M19 9h-4v-6h-6v6h-4l7 7 7-7zm-14 9v2h14v-2h-14z"/></svg>
-      </a>
-    `);
+    this.container = utils.strToEl(
+      '<a href="./" class="floating-action-button">' +
+        '<title>Download output</title>' +
+        '<svg viewBox="0 0 24 24" class="icon"><path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z"/></svg>' +
+      '</a>' +
+    '');
   }
 
   setDownload(filename, url) {
