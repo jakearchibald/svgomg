331bfd4d95472c86b31d85bf67dd58edf48a6971
Jake Archibald <jaffathecake@gmail.com>
2015-01-26 06:57:23 +1100

    Hm, no, iframe data uris don't work in IE

--- src/js/page/ui/svg-output.js
+++ src/js/page/ui/svg-output.js
@@ -2,11 +2,22 @@ var utils = require('../utils');
 
 class SvgOutput {
   constructor() {
-    this.container = utils.strToEl(
-      '<div class="svg-output">' +
-        '<iframe class="svg-container" sandbox="allow-scripts"></iframe>' +
-      '</div>' +
-    '');
+    if (utils.isIe) {
+      // IE doesn't support datauris or blob urls in iframe :(
+      // TODO: feature detect this
+      this.container = utils.strToEl(
+        '<div class="svg-output">' +
+          '<img class="svg-container">' +
+        '</div>' +
+      '');
+    }
+    else {
+      this.container = utils.strToEl(
+        '<div class="svg-output">' +
+          '<iframe class="svg-container" sandbox="allow-scripts"></iframe>' +
+        '</div>' +
+      '');
+    }
 
     this._svgContainer = this.container.querySelector('.svg-container');
   }
@@ -14,8 +25,7 @@ class SvgOutput {
   setSvg(svgFile) {
     // I would rather use blob urls, but they don't work in Firefox
     // All the internal refs break.
-    // IE doesn't support them at all
-    // TODO: file bug reports for both
+    // TODO: file bug reports for this & IE
     var nextLoad = this._nextLoadPromise();
     this._svgContainer.src = "data:image/svg+xml;charset=utf-8;base64," + btoa(svgFile.text);
     this._svgContainer.width = svgFile.width;
