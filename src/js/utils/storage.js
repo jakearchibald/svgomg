84c06e8489637c5111880d843aec04f8fa8f9f5b
Jake Archibald <jaffathecake@gmail.com>
2017-12-04 12:51:26 +0530

    Moving idb-keyval into the repo so it uses the same db as 1.9.

--- src/js/utils/storage.js
+++ src/js/utils/storage.js
@@ -1,6 +1,58 @@
-import idb from 'idb-keyval';
+export let idbKeyval = (() => {
+  let db;
 
-export let idbKeyval = idb;
+  function getDB() {
+    if (!db) {
+      db = new Promise((resolve, reject) => {
+        const openreq = indexedDB.open('svgo-keyval', 1);
+
+        openreq.onerror = () => {
+          reject(openreq.error);
+        };
+
+        openreq.onupgradeneeded = () => {
+          // First time setup: create an empty object store
+          openreq.result.createObjectStore('keyval');
+        };
+
+        openreq.onsuccess = () => {
+          resolve(openreq.result);
+        };
+      });
+    }
+    return db;
+  }
+
+  async function withStore(type, callback) {
+    const db = await getDB();
+    return new Promise((resolve, reject) => {
+      const transaction = db.transaction('keyval', type);
+      transaction.oncomplete = () => resolve();
+      transaction.onerror = () => reject(transaction.error);
+      callback(transaction.objectStore('keyval'));
+    });
+  }
+
+  return {
+    async get(key) {
+      let req;
+      await withStore('readonly', store => {
+        req = store.get(key);
+      });
+      return req.result;
+    },
+    set(key, value) {
+      return withStore('readwrite', store => {
+        store.put(value, key);
+      });
+    },
+    delete(key) {
+      return withStore('readwrite', store => {
+        store.delete(key);
+      });
+    }
+  };
+})();
 
 // iOS add-to-homescreen is missing IDB, or at least it used to.
 // I haven't tested this in a while.
