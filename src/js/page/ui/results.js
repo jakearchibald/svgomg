1ceeed49206b4fe8ec1e8fbdd16c9e8521fa559f
Pierre GIRAUD <pierre.giraud@dalibo.com>
2018-01-18 13:46:46 +0100

    More visual feedback on saving/increase

--- src/js/page/ui/results.js
+++ src/js/page/ui/results.js
@@ -19,16 +19,25 @@ export default class Results {
     this.container = strToEl(
       '<div class="results">' +
         '<span class="size"></span>' +
+        '<span class="spacer"></span>' +
         '<span class="diff"></span>' +
       '</div>' +
     '');
 
     this._sizeEl = this.container.querySelector('.size');
+    this._newSizeEl = this.container.querySelector('.newSize');
     this._diffEl = this.container.querySelector('.diff');
   }
 
   update({ size, comparisonSize }) {
-    this._sizeEl.textContent = humanSize(size);
+    if (comparisonSize) {
+      this._sizeEl.textContent = humanSize(comparisonSize) + ' → ' + humanSize(size);
+    } else {
+      this._sizeEl.textContent = humanSize(size);
+    }
+
+    this._diffEl.classList.remove('success');
+    this._diffEl.classList.remove('error');
 
     // just displaying a single size?
     if (!comparisonSize) {
@@ -36,13 +45,15 @@ export default class Results {
       return;
     }
     else if (size == comparisonSize) {
-      this._diffEl.textContent = ' - no change';
+      this._diffEl.textContent = 'no change';
     }
     else if (size > comparisonSize) {
-      this._diffEl.textContent = ' - ' + round(size / comparisonSize * 100 - 100, 2) + '% increase';
+      this._diffEl.textContent = '+' + round(size / comparisonSize * 100 - 100, 2) + '%';
+      this._diffEl.classList.add('error');
     }
     else {
-      this._diffEl.textContent = ' - ' + round(100 - size / comparisonSize * 100, 2) + '% saving';
+      this._diffEl.textContent = '-' + round(100 - size / comparisonSize * 100, 2) + '%';
+      this._diffEl.classList.add('success');
     }
   }
 }
