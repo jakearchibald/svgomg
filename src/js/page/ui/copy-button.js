53333f8573c8e7f850a5ea7dfcbf11bbcad252fd
XhmikosR <xhmikosr@gmail.com>
2021-09-20 18:10:27 +0300

    Move SVG icons outside of the HTML (#298)

--- src/js/page/ui/copy-button.js
+++ src/js/page/ui/copy-button.js
@@ -7,7 +7,7 @@ export default class CopyButton extends FloatingActionButton {
     super({
       title,
       iconSvg: (
-        '<svg viewBox="0 0 24 24" class="icon">' +
+        '<svg class="icon" viewBox="0 0 24 24">' +
           `<title>${title}</title>` +
           '<path d="M16 1H4C3 1 2 2 2 3v14h2V3h12V1zm3 4H8C7 5 6 6 6 7v14c0 1 1 2 2 2h11c1 0 2-1 2-2V7c0-1-1-2-2-2zm0 16H8V7h11v14z"/>' +
         '</svg>'
