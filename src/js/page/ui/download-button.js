7be1f1d240e8b0e0ce36a3cf5d15df2597dece6a
XhmikosR <xhmikosr@gmail.com>
2021-09-20 08:44:15 +0300

    Move `class` before `viewBox`

--- src/js/page/ui/download-button.js
+++ src/js/page/ui/download-button.js
@@ -8,7 +8,7 @@ export default class DownloadButton extends FloatingActionButton {
       title,
       href: './',
       iconSvg: (
-        '<svg viewBox="0 0 24 24" class="icon">' +
+        '<svg class="icon" viewBox="0 0 24 24">' +
           `<title>${title}</title>` +
           '<path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z"/>' +
         '</svg>'
