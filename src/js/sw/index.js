4ebd7b7da8d6b71e58c4735169077c20622630f7
XhmikosR <xhmikosr@gmail.com>
2021-10-14 14:48:35 +0300

    Revert the code changes

--- src/js/sw/index.js
+++ src/js/sw/index.js
@@ -5,6 +5,8 @@ import {idbKeyval as storage} from '../utils/storage.js';
 const version = SVGOMG_VERSION;
 const cachePrefix = 'svgomg-';
 const staticCacheName = `${cachePrefix}static-${version}`;
+const fontCacheName = `${cachePrefix}fonts`;
+const expectedCaches = [staticCacheName, fontCacheName];
 
 addEventListener('install', event => {
   event.waitUntil((async () => {
@@ -20,6 +22,7 @@ addEventListener('install', event => {
       'js/prism-worker.js',
       'js/svgo-worker.js',
       'changelog.json',
+      'https://fonts.googleapis.com/css?family=Inconsolata',
     ]);
 
     const activeVersion = await activeVersionPromise;
@@ -33,17 +36,36 @@ addEventListener('install', event => {
 
 addEventListener('activate', event => {
   event.waitUntil((async () => {
-    // remove caches beginning "svgomg-" that aren't `staticCacheName`
+    // remove caches beginning "svgomg-" that aren't in expectedCaches
     for (const cacheName of await caches.keys()) {
       if (!cacheName.startsWith(cachePrefix)) continue;
-      if (!cacheName === staticCacheName) await caches.delete(cacheName);
+      if (!expectedCaches.includes(cacheName)) await caches.delete(cacheName);
     }
 
     await storage.set('active-version', version);
   })());
 });
 
+async function handleFontRequest(request) {
+  const match = await caches.match(request);
+  if (match) return match;
+
+  const [response, fontCache] = await Promise.all([
+    fetch(request),
+    caches.open(fontCacheName)
+  ]);
+
+  fontCache.put(request, response.clone());
+  return response;
+}
+
 addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+
+  if (url.host == 'fonts.gstatic.com') {
+    event.respondWith(handleFontRequest(event.request));
+    return;
+  }
   event.respondWith(
     caches.match(event.request).then(r => r || fetch(event.request))
   );
