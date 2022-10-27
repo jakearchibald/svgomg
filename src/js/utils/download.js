ca92db0a1d8c0d191dc01a09b4d9dccf5787d099
XhmikosR <xhmikosr@gmail.com>
2022-10-27 17:11:56 +0300

    Update download.js

--- src/js/utils/download.js
+++ src/js/utils/download.js
@@ -2,14 +2,14 @@ export function download(filename, text) {
   const element = document.createElement('a');
   element.setAttribute(
     'href',
-    'data:text/plain;charset=utf-8,' + encodeURIComponent(text)
+    `data:text/plain;charset=utf-8,${encodeURIComponent(text)}`,
   );
   element.setAttribute('download', filename);
 
   element.style.display = 'none';
-  document.body.appendChild(element);
+  document.body.append(element);
 
   element.click();
 
-  document.body.removeChild(element);
+  element.remove();
 }
