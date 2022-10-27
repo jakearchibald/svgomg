c0ba123bd1a71ac502b480208309a71242d6110f
XhmikosR <xhmikosr@gmail.com>
2022-10-27 17:10:48 +0300

    Tweak gulpfile and build (#398)
    * gulpfile.js: assorted tweaks
    
    * stop minifying on development
    * move most build config related settings under a common object
    * terser: remove comments
    * remove gulp-sourcemaps; support for sourcemaps is built in
    * move functions together
    * exclude maskable.svg from the build folder
    * fix watch html task
    * stop passing all changelog.json contents; we only need the latest version
    
    * gulpfile.js: specify 2 passes for terser as it offers slightly better compression
    
    * gulpfile.js: add clean-css
    
    * gulpfile.js: enable terser's mangle properties option for the page bundle
    
    Only mangle properties whose name starts with an underscore.
    
    Saves ~15% of the uncompressed size, ~7% from the gzip compressed size.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 const fs = require('fs/promises');
 const path = require('path');
+const process = require('process');
 const sirv = require('sirv-cli');
-const svgoPkg = require('svgo/package.json');
+const { version: SVGO_VERSION } = require('svgo/package.json');
 const sass = require('sass');
+const CleanCSS = require('clean-css');
+const vinylMap = require('vinyl-map');
 const gulp = require('gulp');
-const gulpSourcemaps = require('gulp-sourcemaps');
-const gulpSass = require('gulp-sass');
+const gulpif = require('gulp-if');
+const gulpSass = require('gulp-sass')(sass);
 const gulpNunjucks = require('gulp-nunjucks');
 const gulpHtmlmin = require('gulp-htmlmin');
 const rollup = require('rollup');
@@ -14,23 +17,83 @@ const rollupCommon = require('@rollup/plugin-commonjs');
 const rollupReplace = require('@rollup/plugin-replace');
 const { terser: rollupTerser } = require('rollup-plugin-terser');
 
+const IS_DEV_TASK =
+  process.argv.includes('dev') || process.argv.includes('--dev');
+
+const buildConfig = {
+  cleancss: {
+    level: {
+      1: {
+        specialComments: 0,
+      },
+      2: {
+        all: false,
+        mergeMedia: true,
+        removeDuplicateMediaBlocks: true,
+        removeEmpty: true,
+      },
+    },
+    sourceMap: true,
+    sourceMapInlineSources: true,
+  },
+  htmlmin: {
+    collapseBooleanAttributes: true,
+    collapseInlineTagWhitespace: false,
+    collapseWhitespace: true,
+    decodeEntities: true,
+    minifyCSS: false,
+    minifyJS: true,
+    removeAttributeQuotes: true,
+    removeComments: true,
+    removeOptionalTags: true,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    sortAttributes: true,
+    sortClassName: true,
+  },
+  sass: {
+    outputStyle: IS_DEV_TASK ? 'expanded' : 'compressed',
+  },
+  terser: {
+    mangle: true,
+    compress: {
+      passes: 2,
+    },
+    format: {
+      comments: false,
+    },
+  },
+};
+
 const readJSON = async (filePath) => {
   const content = await fs.readFile(filePath, 'utf8');
   return JSON.parse(content);
 };
 
-function css() {
-  const boundSass = gulpSass(sass);
+const minifyCss = vinylMap((buffer) => {
+  return new CleanCSS(buildConfig.cleancss).minify(buffer.toString()).styles;
+});
+
+function copy() {
   return gulp
-    .src('src/css/*.scss')
-    .pipe(gulpSourcemaps.init())
-    .pipe(
-      boundSass
-        .sync({ outputStyle: 'compressed' })
-        .on('error', boundSass.logError),
-    )
-    .pipe(gulpSourcemaps.write('./'))
-    .pipe(gulp.dest('build/'));
+    .src([
+      'src/{.well-known,imgs,test-svgs,fonts}/**',
+      // Exclude the test-svgs files except for `car-lite.svg`
+      // which is used in the demo
+      '!src/test-svgs/!(car-lite.svg)',
+      '!src/imgs/maskable.svg',
+      'src/*.json',
+    ])
+    .pipe(gulp.dest('build'));
+}
+
+function css() {
+  return gulp
+    .src('src/css/*.scss', { sourcemaps: true })
+    .pipe(gulpSass.sync(buildConfig.sass).on('error', gulpSass.logError))
+    .pipe(gulpif(!IS_DEV_TASK, minifyCss))
+    .pipe(gulp.dest('build/', { sourcemaps: '.' }));
 }
 
 async function html() {
@@ -46,32 +109,15 @@ async function html() {
       gulpNunjucks.compile({
         plugins: config.plugins,
         headCSS,
-        changelog,
-        SVGO_VERSION: svgoPkg.version,
+        SVGOMG_VERSION: changelog[0].version,
+        SVGO_VERSION,
         liveBaseUrl: 'https://jakearchibald.github.io/svgomg/',
         title: `SVGOMG - SVGO's Missing GUI`,
         description: 'Easy & visual compression of SVG images.',
         iconPath: 'imgs/icon.png',
       }),
     )
-    .pipe(
-      gulpHtmlmin({
-        collapseBooleanAttributes: true,
-        collapseInlineTagWhitespace: false,
-        collapseWhitespace: true,
-        decodeEntities: true,
-        minifyCSS: false,
-        minifyJS: true,
-        removeAttributeQuotes: true,
-        removeComments: true,
-        removeOptionalTags: true,
-        removeRedundantAttributes: true,
-        removeScriptTypeAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        sortAttributes: true,
-        sortClassName: true,
-      }),
-    )
+    .pipe(gulpif(!IS_DEV_TASK, gulpHtmlmin(buildConfig.htmlmin)))
     .pipe(gulp.dest('build'));
 }
 
@@ -92,15 +138,35 @@ async function js(entry, outputPath) {
       }),
       rollupResolve({ browser: true }),
       rollupCommon({ include: /node_modules/ }),
-      rollupTerser(),
+      // Don't use terser on development
+      IS_DEV_TASK
+        ? ''
+        : rollupTerser(
+            name === 'page'
+              ? {
+                  ...buildConfig.terser,
+                  mangle: {
+                    properties: {
+                      regex: /^_/,
+                    },
+                  },
+                }
+              : buildConfig.terser,
+          ),
     ],
   });
+
   rollupCaches.set(entry, bundle.cache);
+
   await bundle.write({
     sourcemap: true,
     format: 'iife',
     file: `build/${outputPath}/${name}.js`,
   });
+}
+
+function clean() {
+  return fs.rm('build', { force: true, recursive: true });
 }
 
 const allJs = gulp.parallel(
@@ -111,29 +177,13 @@ const allJs = gulp.parallel(
   js.bind(null, 'js/page/index.js', 'js/'),
 );
 
-function copy() {
-  return gulp
-    .src([
-      'src/{.well-known,imgs,test-svgs,fonts}/**',
-      // Exclude the test-svgs files except for `car-lite.svg`
-      // which is used in the demo
-      '!src/test-svgs/!(car-lite.svg)',
-      'src/*.json',
-    ])
-    .pipe(gulp.dest('build'));
-}
-
-function clean() {
-  return fs.rm('build', { force: true, recursive: true });
-}
-
 const mainBuild = gulp.parallel(gulp.series(css, html), allJs, copy);
 
 function watch() {
   gulp.watch(['src/css/**/*.scss'], gulp.series(css, html));
   gulp.watch(['src/js/**/*.js'], allJs);
   gulp.watch(
-    ['src/*.{html,json}', 'src/**/*.{svg,woff2}'],
+    ['src/**/*.{html,svg,woff2}', 'src/*.json'],
     gulp.parallel(html, copy, allJs),
   );
 }
