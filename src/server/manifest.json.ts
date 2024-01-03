aafd69b2426e7b86cada8c78cedb1208f7557e6a
Jake Archibald <jaffathecake@gmail.com>
2024-01-03 09:05:35 +0000

    Manifest

--- src/server/manifest.json.ts
+++ src/server/manifest.json.ts
@@ -1,3 +1,24 @@
+import iconURL from '../client/imgs/icon.png';
+import maskableIconURL from '../client/imgs/maskable.png';
+
 export function manifestJSON() {
-  return { foo: 'bar' };
+  return {
+    name: 'SVGOMG',
+    scope: './',
+    icons: [
+      {
+        src: iconURL,
+        sizes: '600x600',
+      },
+      {
+        src: maskableIconURL,
+        sizes: '600x600',
+        purpose: 'maskable',
+      },
+    ],
+    background_color: '#bababa',
+    display: 'standalone',
+    start_url: './',
+    theme_color: '#303f9f',
+  };
 }
