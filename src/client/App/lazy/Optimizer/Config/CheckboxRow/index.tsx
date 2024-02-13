ef6ec074c19d261aa9fa4d65e3cef6f335cf6f2f
Jake Archibald <jake.archibald@shopify.com>
2024-02-13 08:56:53 +0000

    Menu tidy

--- src/client/App/lazy/Optimizer/Config/CheckboxRow/index.tsx
+++ src/client/App/lazy/Optimizer/Config/CheckboxRow/index.tsx
@@ -2,7 +2,8 @@ import { FunctionComponent } from 'preact';
 import {} from 'preact/hooks';
 import { Signal } from '@preact/signals';
 
-//import * as styles from './styles.module.css';
+import * as styles from './styles.module.css';
+import Rippler from '../../../../Rippler';
 
 interface Props {
   text: string;
@@ -11,16 +12,18 @@ interface Props {
 
 const CheckboxRow: FunctionComponent<Props> = ({ text, checked }) => {
   return (
-    <div>
-      <label>
-        <input
-          type="checkbox"
-          checked={checked}
-          onChange={(event) => (checked.value = event.currentTarget.checked)}
-        />{' '}
-        {text}
-      </label>
-    </div>
+    <Rippler>
+      <div class={styles.checkboxRow}>
+        <label class={styles.label}>
+          <input
+            type="checkbox"
+            checked={checked}
+            onChange={(event) => (checked.value = event.currentTarget.checked)}
+          />{' '}
+          {text}
+        </label>
+      </div>
+    </Rippler>
   );
 };
 
